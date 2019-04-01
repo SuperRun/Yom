ff4254e3bc355a29332e807c63372a842f18be57
VX77 <30616313+VX77@users.noreply.github.com>
2019-04-01 10:56:50 +0200

    Update index.js

--- yom_client/store/index.js
+++ yom_client/store/index.js
@@ -1,7 +1,32 @@
-import newProj from './modules/newProj';
+import Vuex from 'vuex'
 
-export  const namespaced = true;
+const cookieparser = process.server ? require('cookieparser') : undefined
 
-export const modules ={
-    newProj
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      auth: null
+    },
+    mutations: {
+      setAuth(state, auth) {
+        state.auth = auth
+      }
+    },
+    actions: {
+      nuxtServerInit({ commit }, { req }) {
+        let auth = null
+        if (req.headers.cookie) {
+          const parsed = cookieparser.parse(req.headers.cookie)
+          try {
+            auth = JSON.parse(parsed.auth)
+          } catch (err) {
+            // No valid cookie found
+          }
+        }
+        commit('setAuth', auth)
+      }
+    }
+  })
 }
+
+export default createStore
