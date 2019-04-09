0078743155344ebf59b0a85f9673f5fb96793d00
VX77 <30616313+VX77@users.noreply.github.com>
2019-04-09 11:09:24 +0200

    Update index.js

--- yom_client/store/index.js
+++ yom_client/store/index.js
@@ -1,7 +1,23 @@
 import newProj from './modules/newProj';
+import cookieparser from 'cookieparser'
 
 export  const namespaced = true;
 
 export const modules ={
     newProj
+}
+
+export const actions ={
+  nuxtServerInit({ commit }, { req }) {
+    let auth = null
+    if (req.headers.cookie) {
+      const parsed = cookieparser.parse(req.headers.cookie)
+      try {
+        auth = JSON.parse(parsed.auth)
+      } catch (err) {
+        // No valid cookie found
+      }
+    }
+    commit('auth/setAuth', auth)
+  }
 }
