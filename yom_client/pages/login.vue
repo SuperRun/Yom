1bc82498d92a7b0211f76ddeaf7b3e596ba4a2ca
VX77 <30616313+VX77@users.noreply.github.com>
2019-04-09 11:10:08 +0200

    Add files via upload

--- yom_client/pages/login.vue
+++ yom_client/pages/login.vue
@@ -4,7 +4,7 @@
     <v-layout column justify-space-between id="container">
       <v-layout mt-5></v-layout>
       <div align="center">
-      <v-flex xs12>  <v-img src="/login/logo.png" width="50%" ></v-img></v-flex>
+        <v-flex xs12>  <v-img src="/login/logo.png" width="50%" ></v-img></v-flex>
       </div>
       <v-flex xs12>
         <v-layout column
@@ -12,35 +12,33 @@
                   justify-center
                   mt-5
         >
-                <v-flex xs12>
-                  <v-form>
-                      <v-text-field class="mx-auto white--text"
-                                    background-color="rgba(255,250,250,0.5)"
-                                    label="USERNAME"
-                                    box
-                                    required
-                      ></v-text-field>
-                  <v-text-field
-                    v-model="password"
-                    background-color="rgba(255,250,250,0.5)"
-                    :append-icon="show1 ? 'visibility' : 'visibility_off'"
-                    :rules="[rules.required, rules.min]"
-                    :type="show1 ? 'text' : 'password'"
-                    name="input-10-1"
-                    label="PASSWORD"
-                    hint="At least 6 characters"
-                    counter
-                    box
-                    @click:append="show1 = !show1"
-                  ></v-text-field>
-                    <v-layout column
-                              align-center
-                              justify-center
-                    >
-                    <v-btn  color="#3d4047" class="white--text btn-width">login</v-btn>
-                    </v-layout>
-                  </v-form>
-                </v-flex>
+          <v-flex xs12>
+            <v-form>
+              <v-text-field class="mx-auto white--text"
+                            background-color="rgba(255,250,250,0.5)"
+                            label="USERNAME/EMAIL"
+                            solo
+                            v-model="email"
+                            required
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                background-color="rgba(255,250,250,0.5)"
+                label="PASSWORD"
+                required
+                solo
+                :append-icon="show1 ? 'visibility' : 'visibility_off'"
+                :type="show1 ? 'text' : 'password'"
+                @click:append="show1 = !show1"
+              ></v-text-field>
+              <v-layout column
+                        align-center
+                        justify-center
+              >
+                <v-btn  color="#3d4047" class="white--text btn-width" @click="loginClick">login</v-btn>
+              </v-layout>
+            </v-form>
+          </v-flex>
         </v-layout>
       </v-flex>
     </v-layout>
@@ -49,16 +47,36 @@
 </template>
 
 <script>
+  const Cookie = process.client ? require('js-cookie') : undefined
+  import Strapi from 'strapi-sdk-javascript/build/main'
+  const apiUrl = process.env.API_URL || 'http://localhost:1337'
+  const strapi = new Strapi(apiUrl)
+
+
   export default {
+    middleware: 'notAuthenticated',
     data() {
       return {
         show1: false,
         show2: true,
-        password: 'Password',
-        rules: {
-          required: value => !!value || 'Required.',
-          min: v => v.length >= 6 || 'Min 6 characters',
-          userMMatch: () => ('The username and password you entered don\'t match')
+        email:'',
+        password: '',
+      }
+    },
+    methods:{
+      async loginClick(){
+        try {
+          console.log("login Click")
+          const response = await strapi.login(this.email, this.password);
+          const auth = {
+            accessToken: 'response.jwt'
+          };
+          this.$store.commit('auth/setAuth', auth);// mutating to store for client rendering
+          Cookie.set('auth', auth); // saving token in cookie for server rendering
+          this.$router.push(`/userStart`)
+        } catch (err) {
+          this.loading = false;
+          alert(err.message || 'An error occurred.')
         }
       }
     }
@@ -72,7 +90,7 @@
     background-size: cover;
     flex-direction:column;
     justify-content: space-between;
-  }s
+  }
   #container{
     width: 100%;
     height: 100%;
