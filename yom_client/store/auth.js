5d56cc60446166fc0cbe54cadb39f8b4fe8afcc5
VX77 <30616313+VX77@users.noreply.github.com>
2019-04-10 11:54:01 +0200

    Add files via upload

--- yom_client/store/auth.js
+++ yom_client/store/auth.js
@@ -1,10 +1,14 @@
 const cookieparser = process.server ? require('cookieparser') : undefined
 const state = () => ({
-  auth:null
+  auth:null,
+  id:null
 })
 const mutations = {
   setAuth(state, auth) {
     state.auth = auth
+  },
+  setId(state, id) {
+    state.id = id
   }
 }
 export default {
